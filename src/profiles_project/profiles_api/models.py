ac1c3ed5eb725dcfd69dab72d225317951657da3
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-04 18:40:06 +0300

    create user model in models.py

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """ Helps Django to work with our UserModel """
+
+    def create_user(self, email=None, name=None, password=None):
+        if not email:
+            raise ValueError('User must have an email')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, name=None, password=None):
+        superuser = self.create_user(email=email, name=name, password=password)
+        superuser.is_superuser = True
+        superuser.is_staff = True
+        superuser.save(using=self._db)
+        return superuser
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """ Represents user profile inside system """
+    email = models.EmailField(max_length=255, unique=True)
+    name  = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """ Django uses it to get a user full name """
+        return self.name
+
+    def get_short_name(self):
+        """Django uses this when it needs to get the users abbreviated name."""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
+
+        return self.name + " : " + self.email
