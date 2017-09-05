c5a92471ad727d127a43484269e0f823e51d6655
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-05 19:04:26 +0300

    add profile api modelviewset

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,4 +1,22 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class NameSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):   # create and return new user
+        user = models.UserProfile(
+            email=validated_data['email'].lower(),
+            name=validated_data['name'],
+            )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from rest_framework.views  import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from profiles_api import serializers
 
-from rest_framework import viewsets
+from profiles_api import models
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+
+
+
 
 class HelloViewSet(viewsets.ViewSet):
 
